6a087c0bed876c22de22583c96435f52cdb3b91d
DANIEL WILLIAMS <danny@uk.ibm.com>
2017-03-10 05:35:49 -0600

    Changed IoT Platform Service Name in app.js

--- app.js
+++ app.js
@@ -43,9 +43,9 @@ var iotCredentials;
 
 /**
  * @attention 
- * iot-raspberrypi is the IoT platform service name in this example, you should replace it with yours
+ * iot-raspberrypi is the IoT platform service name in this example, you should replace it with yours - DONE
  */
-var iotPlatformServiceName = 'iot-raspberrypi';
+var iotPlatformServiceName = 'Internet of Things Platform-l5';
 
 //Loop through configuration internally defined in Bluemix and retrieve the credential from the IoT service
 var baseConfig = appEnv.getServices(iotPlatformServiceName);
